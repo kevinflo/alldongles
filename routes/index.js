bc01fc193a5d5b0942ff29c9a81f85b995076f6c
kevinflo <kevinflo@gmail.com>
2017-09-11 22:23:56 -0700

    slight changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'AllDongles.com - Best dongles for all your devices - MacBook Pro iPhone Android Samsung'
+  });
 });
 
 module.exports = router;
