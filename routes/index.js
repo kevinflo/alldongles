c4f673615ae5bea237ec05ee11d2147f57c2403d
kevinflo <kevinflo@gmail.com>
2017-09-13 00:17:54 -0700

    added semantic

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,11 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/semantic', function(req, res, next) {
+    res.render('semantic', {
+        title: 'AllDongles.com - Best dongles for all your devices - MacBook Pro iPhone Android Samsung'
+    });
+});
+
+
 module.exports = router;
